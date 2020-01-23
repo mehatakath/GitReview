4afb62b7bceb99a58e1fd8a2b9ee35b34ad6cf72
mehatakat <mehtakat@sheridanc.on.ca>
2020-01-23 14:43:36 -0500

    First Commit

---
+++ src/gitreview/GitReview.java
@@ -0,0 +1,26 @@
+ /*
+        Name:  [your name here]
+        Assignment:  [assignment name]
+        Program: [your program name here]
+        Date:  [assignment due date here]
+    
+        Description:
+        [program description in your own words]
+    */
+package gitreview;
+
+/**
+ *
+ * @author Katrina
+ */
+public class GitReview {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+        System.out.println("Git Practice");
+    }
+
+}
